11441d09e4fcbcd509b7cf301c2f7dcbe5d6cc4b
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2024-07-10 06:21:55 +0000

    f

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+       <h1>Welcome to My React App</h1>
+       <p>I am learning React. My life is getting better.</p>
+    </div>
   )
 }
 
